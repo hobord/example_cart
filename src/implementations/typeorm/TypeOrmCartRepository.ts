2e48a7157fe3da4bd692a2c92290c1200a19736f
Balázs Szabó <balazs.szabo@gmail.com>
2018-10-21 13:43:57 +0300

    queryRunner.startTransaction

--- src/implementations/typeorm/TypeOrmCartRepository.ts
+++ src/implementations/typeorm/TypeOrmCartRepository.ts
@@ -5,6 +5,7 @@ import { CartDbModel } from "./entities/CartDbModel";
 import { ICartFactory } from "../../interfaces/ICartFactory";
 import { CartFactoryDbModelDecorated } from "./CartFactoryDbModelDecorated";
 import { CartLineDbModelFactory } from "./CartLineDbModelFactory";
+import { CartLineDbModel } from "./entities/CartLineDbModel";
 
 export class TypeOrmCartRepository implements ICartRepository {
   protected cartFactory: CartFactoryDbModelDecorated;
@@ -43,20 +44,31 @@ export class TypeOrmCartRepository implements ICartRepository {
     const cartLines = cart.getCartLines();
 
     return new Promise<ICart>(async resolve => {
-      await this.connection.transaction(async transactionalEntityManager => {
-        cartDbModel = await transactionalEntityManager.save(cartDbModel);
+      const queryRunner = this.connection.createQueryRunner();
+      await queryRunner.startTransaction();
+      
+      try {
+        cartDbModel = await queryRunner.manager.save(cartDbModel);
+        await this.connection.createQueryBuilder()
+          .delete()
+          .from(CartLineDbModel)
+          .where("cartId = :cartId", { cartId: cartDbModel.id }).execute();
+
         for (let index = 0; index < cartLines.length; index++) {
           const cartLine = cartLines[index];
           const cartLineDbModel = this.cartLineDbModelFactory.createWithCartIdFromCartLine(
             cartDbModel.id,
             cartLine
           );
-          await transactionalEntityManager.save(cartLineDbModel)
+          await queryRunner.manager.save(cartLineDbModel)
         }
-      }).catch(error => {
-        throw new Error("Method not implemented." + error);
-      });
 
+      } catch (error) {
+        await queryRunner.rollbackTransaction();
+        throw new Error("Method not implemented." + error);
+      }
+
+      await queryRunner.commitTransaction();
       resolve(this.getById(cartDbModel.id));
     });
   }
